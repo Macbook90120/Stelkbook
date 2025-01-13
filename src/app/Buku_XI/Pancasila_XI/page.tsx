accb2b8d209a7f9d059852f0302d1a4f9191f2e7
Macbook90120 <coolmacren@gmail.com>
2025-01-13 12:26:53 +0800

    jjjj

--- src/app/Buku_XI/Pancasila_XI/page.tsx
+++ src/app/Buku_XI/Pancasila_XI/page.tsx
@@ -35,6 +35,7 @@ function Page() {
   };
 
   return (
+<<<<<<< HEAD
     <>
       <head>
         <link
@@ -170,6 +171,46 @@ function Page() {
       </div>
     </>
   );
+=======
+    <div>
+      <h1>Buku Pancasila Kelas XII</h1>
+      <p>This is the content for Buku Pancasila Kelas XII. Laso</p>
+Makna dan Hakikat Ideologi
+
+1. Pengertian Ideologi
+
+Ideologi berasal dari kata idea yang berarti 'gagasan, konsep, pengertian dasar, cita-cita'; sementara logos berarti 'ilmu. Secara harfiah, ideologi berarti 'ilmu pengetahuan mengenai ide-ide atau gagasan. Terdapat beberapa pengertian mengenai ideologi oleh para ahli, antara lain sebagai berikut.
+
+a. Padmo Wahjono (1991) menyatakan bahwa ideologi adalah kesatuan yang bulat dan utuh dari ide-ide dasar.
+
+b. Menurut W. White, ideologi merupakan asas pendapat, pandangan, atau keyakinan yang dipakai atau dicita-citakan oleh perorangan atau golongan dalam masyarakat atau oleh suatu bangsa (Ishaq, 2021).
+
+c. A. S. Hornby mengatakan bahwa ideologi adalah seperangkat gagasan yang membentuk landasan teori ekonomi dan politik atau yang dipegangi oleh seorang atau sekelompok orang (Herdiawanto, dkk., 2019).
+
+d. Manfred B. Steger (2003) mendefinisikan ideologi sebagai sistem ide-ide yang dimiliki bersama secara luas, keyakinan yang berpola, norma-norma dan nilai-nilai yang menjadi pedoman, dan cita-cita yang diterima sebagai kebenaran oleh sekelompok orang tertentu.
+
+e. Menurut Mubyarto, ideologi adalah sejumlah doktrin, kepercayaan, dan simbol-simbol kelompok masyarakat atau suatu bangsa yang menjadi pegangan dan pedoman kerja atau perjuangan untuk mencapai tujuan masyarakat bangsa (Ishaq. 2021).
+
+1. David Miller (Kusumohamidjojo, 2015) menyebut ideologi sebagai seperangkat keyakinan alam sosial dan politik yang secara bersamaan memberi makna kepada sesuatu yang berlangsung dalam masyarakat dan membimbing respons praktis masyarakat terhadapnya.
+
+Berdasarkan berbagai definisi tersebut, terdapat beberapa unsur yang memberi batasan tentang ideologi, yaitu sebagai berikut (Herdiawanto, dkk., 2019).
+
+a. Sekumpulan ide atau gagasan.
+
+b. Tersusun secara sistematis.
+
+c. Bersumber dari pikiran manusia.
+
+d. Mempunyai tujuan dan arah yang jelas,
+
+e. Pedoman tentang cara hidup.
+
+F. Dianut oleh masyarakat 
+    </div>
+
+
+  )
+>>>>>>> b47563c16c10e31acb94e1d49df847c879679dd7
 }
 
 export default Page;
