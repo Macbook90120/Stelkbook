fe4cb692e17fbc1b4874bb7526d93daca8b558e3
Macbook90120 <coolmacren@gmail.com>
2025-02-09 11:48:30 +0800

    commit lagiiii

--- src/app/admin/Create_User/page.tsx
+++ src/app/admin/Create_User/page.tsx
@@ -93,9 +93,9 @@ function Page() {
 
           {/* Profile Details */}
           <div className="grid gap-4 w-full">
-            {/* Nama Field */}
+            {/* Username Field */}
             <div>
-              <label className="block text-gray-700 text-sm font-medium mb-2">Nama</label>
+              <label className="block text-gray-700 text-sm font-medium mb-2">Username</label>
               <input
                 type="text"
                 defaultValue=""
@@ -106,6 +106,16 @@ function Page() {
             {/* Email Field */}
             <div>
               <label className="block text-gray-700 text-sm font-medium mb-2">Email</label>
+              <input
+                type="text"
+                defaultValue=""
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+             {/* Password Field */}
+             <div>
+              <label className="block text-gray-700 text-sm font-medium mb-2">Password</label>
               <input
                 type="text"
                 defaultValue=""
--- src/app/admin/Data_Siswa/Edit_user/page.tsx
+++ src/app/admin/Data_Siswa/Edit_user/page.tsx
@@ -108,9 +108,9 @@ function Page() {
 
           {/* Profile Details */}
           <div className="grid gap-4 w-full">
-            {/* Nama Field */}
+            {/* Username Field */}
             <div>
-              <label className="block text-gray-700 text-sm font-medium mb-2">Nama</label>
+              <label className="block text-gray-700 text-sm font-medium mb-2">Username</label>
               <input
                 type="text"
                 defaultValue=""
@@ -121,6 +121,16 @@ function Page() {
             {/* Email Field */}
             <div>
               <label className="block text-gray-700 text-sm font-medium mb-2">Email</label>
+              <input
+                type="text"
+                defaultValue=""
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Password Field */}
+            <div>
+              <label className="block text-gray-700 text-sm font-medium mb-2">Password</label>
               <input
                 type="text"
                 defaultValue=""
