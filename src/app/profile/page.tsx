c6d7134ced8f893a174ae2aaa406fdf697d3d05a
Macbook90120 <coolmacren@gmail.com>
2025-02-11 09:16:14 +0800

    update stelk

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -68,6 +68,15 @@ function Page() {
                   className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
+              <div>
+                <label className="block text-gray-700 text-sm font-medium mb-2">Sekolah</label>
+                <input
+                  type="text"
+                  defaultValue=""
+                  readOnly
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div>
                   <label className="block text-gray-700 text-sm font-medium mb-2">Status</label>
