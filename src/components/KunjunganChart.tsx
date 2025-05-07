85c29a0179b7484a79a23451516bfbcb7892e2cf
UgreZz <ugrasena0421@gmail.com>
2025-05-07 20:26:04 +0800

    fix bug chartbar

--- src/components/KunjunganChart.tsx
+++ src/components/KunjunganChart.tsx
@@ -17,7 +17,7 @@ interface ChartProps {
 
 export default function KunjunganChart({ data }: ChartProps) {
   return (
-    <ResponsiveContainer width="100%" height="100%">
+    <ResponsiveContainer width="100%" height={300}>
       <BarChart data={data}>
         <defs>
           <linearGradient id="barGradient" x1="0" y1="0" x2="0" y2="1">
